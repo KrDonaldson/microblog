d1e1ba853f6b52830c92d67afda2f930cf3c721a
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-02-27 14:40:13 -0700

    first database migration

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=microblog.py
+SQLALCHEMY_DATABASE_URI = postgresql://kibxyqnv:hiFVohd8TIH_giGgUyKR446b8b9YJ6rk@kashin.db.elephantsql.com/kibxyqnv
--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from app import db
+from datetime import datetime as dt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
+    created_on = db.Column(db.DateTime, default = dt.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
