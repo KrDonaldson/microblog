fd01e70f0e28bb774826da033413776015f99a6c
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-02-26 22:27:42 -0700

    add login form

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app
 from flask import render_template
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'test'}
     posts = [
@@ -17,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title ='Sign In', form=form)
